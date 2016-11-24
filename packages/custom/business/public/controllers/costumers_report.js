60b23dca4c10c9be19e78d30c9b7891260124ad0
Turupawn <ahmed.hn.43@gmail.com>
2016-11-24 10:55:11 -0600

    costumers report progress

--- packages/custom/business/public/controllers/costumers_report.js
+++ packages/custom/business/public/controllers/costumers_report.js
@@ -10,6 +10,11 @@
         };
 
 				$scope.title = 'Costumers Report T';
+        $scope.costumers = [{rank:1, points: 1500, name: 'Juan', email: 'juan@gmail.com'},
+                            {rank:2, points: 1200, name: 'Ana', email: 'ana@gmail.com'},
+                            {rank:3, points: 900, name: 'Pedro', email: 'pedro@gmail.com'}
+                           ];
+
     }
 
     angular
